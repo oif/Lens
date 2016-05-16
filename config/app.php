bba965860f7dfd095230117447d00cc4138ca439
oif <neowiz@icloud.com>
2016-05-16 23:00:51 +0800

    today's final commit

---
+++ config/app.php
@@ -0,0 +1,14 @@
+<?php
+
+// 应用目录为当前目录
+define('APP_PATH', __DIR__.'/');
+
+// 开启调试模式
+define('APP_DEBUG', true);
+
+
+// 数据库
+define('DB_HOST', 'localhost');
+define('DB_NAME', 'meituDB');
+define('DB_USER', 'neo');
+define('DB_PASSWORD', 'root');
--- index.php
+++ index.php
@@ -1,3 +1,3 @@
 <?php
-
-require './framework/Lens.php';
+require './config/app.php';	// 载入配置文件
+require './framework/Lens.php';	// 载入核心文件
