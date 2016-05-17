d076350e256de1841e3ba9d8763a81256d174ae9
oif <neowiz@icloud.com>
2016-05-17 22:48:07 +0800

    update readme

--- app.php
+++ app.php
@@ -11,8 +11,8 @@ define('TOKEN_EXP', 3660); // 单位：秒
 
 
 // 数据库
-define('DB_HOST', 'localhost');
-define('DB_NAME', 'meitu');
-define('DB_USER', 'homestead');
-define('DB_PASSWORD', 'secret');
+define('DB_HOST', 'localhost');	// 服务器地址
+define('DB_NAME', 'meitu');	// 数据库名
+define('DB_USER', 'homestead');	// 数据库用户
+define('DB_PASSWORD', 'secret');	// 数据库密码
 
