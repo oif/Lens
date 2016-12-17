6af2391db388035c63af2505a76a5027cfb2eded
oif <neowiz@icloud.com>
2016-12-17 12:11:00 +0800

    调整数据库单例模式

--- app/model/Model.php
+++ app/model/Model.php
@@ -1,8 +1,11 @@
 <?php
 
-class Model extends SQL {
+class Model {
 	protected $properties = array();
 	protected $table;
+    protected $db;
+    protected $_result;
+
 
 	public function __get($property) {
 		if (array_key_exists($property, $this->properties)) {	// 检测是否存在
@@ -14,17 +17,40 @@ class Model extends SQL {
 		$this->properties[$property] = $value;
 	}
 
-	function __construct() {
-		// 连接数据库
-        $this->connect(DB_HOST, DB_USER, DB_PASSWORD, DB_NAME);
+    function __construct() {
+        // 获取数据库实例
+        $this->db = SQL::getInstance();
 
         // 根据类名获得表名，命名规则例如 User 则表名为 users
         $this->table = strtolower(get_class($this).'s');
 	}
 
-	function __destruct() {
-		$this->disconnect();	// 释放数据库
-	}
+    function selectAll() {	// 所有结果
+        $query = 'select * from `'.$this->table.'`';
+        return $this->query($query);
+    }
+
+    function select($id) {	//	根据 ID 查询
+        $query = 'select * from `'.$this->table.'` where `id` = \''.$id.'\'';
+        return $this->query($query);
+    }
+
+    function delete($id) {	// 根据 ID 删除
+        $query = 'delete from `'.$this->table.'` where `id` = \''.$id.'\'';
+        return $this->query($query);
+    }
+
+    function query($query) {	// SQL
+        $this->_result = self::$_dbHandle->query($query);
+        if (preg_match("/select/i",$query)) {
+            return $this->_result->fetch_array();
+        }
+        return $this->_result;
+    }
+
+    function getError() {	// 错误信息
+        return mysqli_error($this->db);
+    }
 
 }
 
